39870094d909e783f88dfd5fd86beffe83043dcf
Ankit Kanojia <ankit.kanojia@devdigital.com>
2021-01-04 19:09:34 +0530

    Handlig student object and create APIs for fetch records of students.

--- server/resolvers.js
+++ server/resolvers.js
@@ -3,7 +3,25 @@ const { DataStore } = require('notarealdb');
 const store = new DataStore('./data');
 
 const Query = {
-   test: () => store.collection('students').list()
+   greeting: () => {
+      return "hello from  TutorialsPoint !!!"
+   },
+   students: () => store.collection('students').list(),
+   studentById: (root, args, context, info) => {
+      return store.collection('students').get(args.id);
+   },
+   limitedStudents: (root, args, context, info) => {
+      return store.collection('students').list().slice(0, args.first)
+   },
+   sortedStudents: (root, args, context, info) => {
+      return store.collection('students').list();
+   }
 }
 
-module.exports = { Query }   
+const Student = {
+   fullName:(root,args,context,info) => {
+      return root.firstName + ":" + root.lastName
+   }
+}
+
+module.exports = { Query, Student }   
